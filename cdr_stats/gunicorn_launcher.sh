5b4a4d77b9691a1b157380fedec53876bc3d62fb
Jenner Micheal <631475170@qq.com>
2019-04-25 14:54:50 +0800

    Revert "本地修改"
    This reverts commit 781b36d0e7859192825e844e89368e7c06bc33f9.

--- cdr_stats/gunicorn_launcher.sh
+++ cdr_stats/gunicorn_launcher.sh
@@ -7,7 +7,7 @@ LOGDIR=$(dirname $LOGFILE)
 # You can set it as low as 1 if you’re on a small VPS.
 # A popular formula is 1 + 2 * number_of_cpus on the machine (the logic being,
 # half of the processess will be waiting for I/O, such as database).
-NUM_WORKERS=4
+NUM_WORKERS=1
 
 # user/group to run as
 USER=cdr_stats
@@ -15,8 +15,7 @@ GROUP=cdr_stats
 
 # cd /usr/share/virtualenvs/cdr-stats
 # source bin/activate
-# source /opt/miniconda/envs/cdr-stats/bin/activate /opt/miniconda/envs/cdr-stats
-source activate  /opt/miniconda/envs/cdr-stats
+source /opt/miniconda/envs/cdr-stats/bin/activate /opt/miniconda/envs/cdr-stats
 cd /usr/share/cdrstats
 
 test -d $LOGDIR || mkdir -p $LOGDIR
@@ -25,7 +24,3 @@ test -d $LOGDIR || mkdir -p $LOGDIR
 exec gunicorn cdr_stats.wsgi:application -b 127.0.0.1:8123 -w $NUM_WORKERS --timeout=300 \
     --user=$USER --group=$GROUP --log-level=debug \
     --log-file=$LOGFILE 2>>$LOGFILE
-
-
-
-# exec gunicorn cdr_stats.wsgi:application -b 127.0.0.1:8123 -w 1 --timeout=300 --user=cdr_stats --group=cdr_stats --log-level=debug --log-file=/var/log/cdr-stats/gunicorn_cdr_stats.log
